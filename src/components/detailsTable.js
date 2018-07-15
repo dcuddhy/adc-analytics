c16a2cfa9e75963de8d03f28ad97a66a37f1d2ad
dcuddhy <dcuddhy@gmail.com>
2018-07-14 19:05:35 -0600

    Able to log data, but not printing data yet.

--- src/components/detailsTable.js
+++ src/components/detailsTable.js
@@ -3,8 +3,30 @@ import './detailsTable.css';
 
 
 class DetailsTable extends Component {
-  // This is where we will display table.
-  render() {
+  constructor() {
+    super();
+    this.state = {
+        dataRows: []
+    };
+}
+  // I ran into a CORS issue when trying to run server locally.  Became easier to
+  // set up free online JSON server for sake of speed.
+  componentDidMount() {
+    fetch('//api.jsonbin.io/b/5b4a8b29dd2c022ecda27a5c')
+    .then(results => {
+        return results.json();
+    }).then(data => {
+        // Complete list of users of all users
+        let dataRows = data.rows;
+        this.setState({dataRows: dataRows});
+        console.log('state0: ', this.state.dataRows);
+
+    }).catch(function() {
+        console.log("Fetching data failed at UserList.componentDidMount()");
+    });
+}
+
+render() {
     return (
       <div className="display-table">
         display table <br /> <br />
---
+++ src/components/detailsTableRow.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './detailsTable.css';
+
+
+class detailsTableRow extends Component {
+
+  render() {
+    return (
+      <div className="display-table">
+        display table row<br /> <br />
+
+      </div>
+    );
+  }
+}
+
+export default detailsTableRow;
